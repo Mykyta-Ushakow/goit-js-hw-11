18bea93358af83327d2e041775be535a42587fa7
Mykyta-Ushakow <nilitosushak@gmail.com>
2023-10-16 02:22:09 +0300

    Checkpoint 2

--- src/index.js
+++ src/index.js
@@ -19,46 +19,84 @@
     5.3 Зробити плавне прокручування сторінки після запиту і відтворення кожної наступної групи зображень.
     5.4 Замість кнопки «Load more», можна зробити нескінченне завантаження зображень під час прокручування сторінки. Ми надаємо тобі повну свободу дій в реалізації, можеш використовувати будь-які бібліотеки.
 
-
-
-
 */
 
 import axios from "axios";
 import Notiflix from "notiflix";
 
-const page = 1;
+let page = 1;
+const perPage = 40;
 
 const refs = {
     submitBtn: document.querySelector(".submit-btn"),
     searchForm: document.querySelector(".search-form"),
+    galleryEl: document.querySelector(".gallery"),
     loadMore: document.querySelector(".load-more")
 }
 
-const { submitBtn, searchForm, loadMore } = refs;
-
-
-console.log(submitBtn);
-console.log(searchForm);
-console.log(loadMore);
+const { galleryEl, submitBtn, searchForm, loadMore } = refs;
 
 searchForm.addEventListener("submit", imagesService);
+loadMore.addEventListener("click", handleLoadMore);
 
 function imagesService(event) {
     event.preventDefault();
+    submitBtn.disabled = true;
+    loadMore.classList.add('hidden');
 
+    page = 1;
+        
     const submitData = new FormData(event.currentTarget);
-    const qValue = submitData.get("searchQuery");
+    const qValue = submitData.get("searchQuery").trim();
 
-    console.log(getImages(qValue));
+    if (!qValue) {
+        Notiflix.Notify.warning("Please enter your query", {
+                width: '500px',
+                svgSize: '120px',
+                clickToClose: true,
+                pauseOnHover: true,
+                fontSize: '26px',
+        });
+        submitBtn.disabled = false;
+        return;
+    }
 
-    // for (const [key, value] of submitData) {
-    //     console.log(`${key}: ${value}\n`);
-    // }
+    getImages(qValue)
+        .then(resp => {
+
+            console.log(resp);
+            console.log(resp.data);
+            console.log(resp.data.hits);
+            console.log(resp.data.totalHits);
+            
+
+            if (resp.data.hits.length === 0) {
+                throw new Error('Sorry, there are no images matching your search query. Please try again.');
+            }
+        
+            const markup = createMarkup(resp.data.hits);
+            galleryEl.innerHTML = markup;
+            
+            if (resp.data.totalHits > perPage) {
+                loadMore.classList.remove('hidden');   
+            }       
+        
+        })
+        .catch(err => {
+            Notiflix.Notify.failure(`${err}`, {
+                width: '500px',
+                svgSize: '120px',
+                clickToClose: true,
+                pauseOnHover: true,
+                fontSize: '26px',
+            })
+        })
+        .finally(() => {
+            submitBtn.disabled = false;
+        });
 }
 
 async function getImages(qValue) {
-    
     const BASE_URL = "https://pixabay.com/api/"
     const URL_PARAMS = new URLSearchParams({
         key: "40067748-747d8141c6ab6be87462ec83d",
@@ -66,9 +104,84 @@ async function getImages(qValue) {
         image_type: "photo",
         orientation: "hprizontal",
         safesearch: true,
+        page: page,
+        per_page: perPage,
     });
-    
-    const resp = await axios.get(`${BASE_URL}?${URL_PARAMS}`);
-
-    return resp;
+        
+    return await axios.get(`${BASE_URL}?${URL_PARAMS}`);
 }
+
+function createMarkup(arr) {
+
+    return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+    <div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+            <p class="info-item">
+                ${likes}
+                <b>Likes</b>
+            </p>
+            <p class="info-item">
+                ${views}
+                <b>Views</b>
+            </p>
+            <p class="info-item">
+                ${comments}
+                <b>Comments</b>
+            </p>
+            <p class="info-item">
+                ${downloads}
+                <b>Downloads</b>
+            </p>
+        </div>
+    </div>
+    `).join("");
+}
+
+function handleLoadMore(event) {
+    event.preventDefault();
+    page++;
+
+    const submitData = new FormData(searchForm);
+    const qValue = submitData.get("searchQuery");
+
+    getImages(qValue)
+        .then(resp => {
+
+            console.log(resp);
+            console.log(resp.data);
+            console.log(resp.data.hits);
+        
+            const markup = createMarkup(resp.data.hits);
+            galleryEl.insertAdjacentHTML("beforeend", markup);
+
+            if (galleryEl.children.length >= resp.data.totalHits) {
+                loadMore.classList.add("hidden");
+                Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.", {
+                width: '500px',
+                svgSize: '120px',
+                clickToClose: true,
+                pauseOnHover: true,
+                fontSize: '26px',
+            })
+            }
+
+        })
+        .catch(err => {
+
+            Notiflix.Notify.failure(`${err}`, {
+                width: '500px',
+                svgSize: '120px',
+                clickToClose: true,
+                pauseOnHover: true,
+                fontSize: '26px',
+            })
+        });
+}
+
+
+// window.onscroll = function(ev) {
+//     if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+//         alert("you're at the bottom of the page");
+//     }
+// };
